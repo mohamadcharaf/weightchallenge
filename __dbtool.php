34e0a1525e52d45247b9ac1cd86aff5fb8efaf20
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-08-30 15:39:51 -0500

    Tool for editing DB
    And for keeping track of DB changes

---
+++ __dbtool.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "eap-app-mysql";
+$username = "userx4C";
+$password = "1huOBjL5";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "<br>Connected successfully";
+
+echo "<br>Trying to create table";
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(15) NOT NULL,
+  `user_email` varchar(40) NOT NULL,
+  `user_pass` varchar(255) NOT NULL,
+  `joining_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+echo "<br>Table created";
+
+
+?>
