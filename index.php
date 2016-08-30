e50728798427c7e8cabb78ae6dfe3daa595873c2
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-08-30 18:57:19 -0500

    Again

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
 session_start();
-require_once("class.user.php");
+require_once( 'class.user.php' );
 $login = new USER();
 
 if($login->is_loggedin()!="")
 {
-	$login->redirect('home.php');
+  $login->redirect('home.php');
 }
 
 if(isset($_POST['btn-login']))
 {
-	$uname = strip_tags($_POST['txt_uname_email']);
-	$umail = strip_tags($_POST['txt_uname_email']);
-	$upass = strip_tags($_POST['txt_password']);
-		
-	if($login->doLogin($uname,$umail,$upass))
-	{
-		$login->redirect('home.php');
-	}
-	else
-	{
-		$error = "Wrong Details !";
-	}	
+  $uname = strip_tags($_POST['txt_uname_email']);
+  $umail = strip_tags($_POST['txt_uname_email']);
+  $upass = strip_tags($_POST['txt_password']);
+
+  if($login->doLogin($uname,$umail,$upass))
+  {
+    $login->redirect('home.php');
+  }
+  else
+  {
+    $error = "Wrong Details !";
+  }
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -37,48 +37,48 @@ if(isset($_POST['btn-login']))
 
 <div class="signin-form">
 
-	<div class="container">
-     
-        
+  <div class="container">
+
+
        <form class="form-signin" method="post" id="login-form">
-      
+
         <h2 class="form-signin-heading">Log In to WebApp.</h2><hr />
-        
+
         <div id="error">
         <?php
-			if(isset($error))
-			{
-				?>
+      if(isset($error))
+      {
+        ?>
                 <div class="alert alert-danger">
                    <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
                 </div>
                 <?php
-			}
-		?>
+      }
+    ?>
         </div>
-        
+
         <div class="form-group">
         <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or E mail ID" required />
         <span id="check-e"></span>
         </div>
-        
+
         <div class="form-group">
         <input type="password" class="form-control" name="txt_password" placeholder="Your Password" />
         </div>
-       
-     	<hr />
-        
+
+      <hr />
+
         <div class="form-group">
             <button type="submit" name="btn-login" class="btn btn-default">
-                	<i class="glyphicon glyphicon-log-in"></i> &nbsp; SIGN IN
+                  <i class="glyphicon glyphicon-log-in"></i> &nbsp; SIGN IN
             </button>
-        </div>  
-      	<br />
+        </div>
+        <br />
             <label>Don't have account yet ! <a href="sign-up.php">Sign Up</a></label>
       </form>
 
     </div>
-    
+
 </div>
 
 </body>
