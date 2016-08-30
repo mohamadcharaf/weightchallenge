b6c4500e1feb07d878ed02365938039b964dbb7f
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-08-30 16:58:40 -0500

    Checking more variables

--- __index.php
+++ __index.php
@@ -1,11 +1,17 @@
 <?php
 define( 'DB_HOST', getenv( 'OPENSHIFT_MYSQL_DB_HOST' ) );
-define( 'DB_NAME', getenv( 'OPENSHIFT_GEAR_NAME' ) );
 define( 'DB_PORT', getenv( 'OPENSHIFT_MYSQL_DB_PORT' ) );
+define( 'DB_USER', getenv( 'OPENSHIFT_MYSQL_DB_USERNAME' ) );
+define( 'DB_PASS', getenv( 'OPENSHIFT_MYSQL_DB_PASSWORD' ) );
+define( 'DB_SOCK', getenv( 'OPENSHIFT_MYSQL_DB_SOCKET' ) );
+define( 'DB_URL', getenv( 'OPENSHIFT_MYSQL_DB_URL' ) );
 
 echo '<br>Hello World';
-echo '<br>DB_HOST' . DB_HOST;
-echo '<br>DB_NAME' . DB_NAME;
-echo '<br>DB_PORT' . DB_PORT;
+echo '<br>DB_HOST: ' . DB_HOST;
+echo '<br>DB_PORT: ' . DB_PORT;
+echo '<br>DB_USER: ' . DB_USER;
+echo '<br>DB_PASS: ' . 'No, not going to show you the pw';
+echo '<br>DB_SOCK: ' . DB_SOCK;
+echo '<br>DB_URL: ' . DB_URL;
 phpinfo();
 ?>
