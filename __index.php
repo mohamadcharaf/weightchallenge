4cb926e646e864d7551547a42c364b9b4bfd7d44
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-08-30 16:42:27 -0500

    Test environment variables

--- __index.php
+++ __index.php
@@ -1,4 +1,11 @@
 <?php
-echo 'Hello World';
+define( 'DB_HOST', getenv( 'OPENSHIFT_MYSQL_DB_HOST' ) );
+define( 'DB_NAME', getenv( 'OPENSHIFT_GEAR_NAME' ) );
+define( 'DB_PORT', getenv( 'OPENSHIFT_MYSQL_DB_PORT' ) );
+
+echo '<br>Hello World';
+echo '<br>DB_HOST' . DB_HOST;
+echo '<br>DB_NAME' . DB_NAME;
+echo '<br>DB_PORT' . DB_PORT;
 phpinfo();
 ?>
