e361f9d99c6ce1901dcca0d49aceebaee3b75bea
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-08-30 15:43:20 -0500

    User table creation added

--- __dbtool.php
+++ __dbtool.php
@@ -13,14 +13,17 @@ if ($conn->connect_error) {
 echo "<br>Connected successfully";
 
 echo "<br>Trying to create table";
-CREATE TABLE IF NOT EXISTS `users` (
+
+$sql = "CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(15) NOT NULL,
   `user_email` varchar(40) NOT NULL,
   `user_pass` varchar(255) NOT NULL,
   `joining_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;";
+$stmt = $this->conn->prepare( $sql );
+$stmt->execute();
 echo "<br>Table created";
 
 
