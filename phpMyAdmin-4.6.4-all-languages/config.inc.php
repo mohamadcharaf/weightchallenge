f629be2c26f8df745ca59f14b81996e27325707b
Mohamad Charaf <mohamad.charaf@gmail.com>
2016-08-30 05:35:09 -0500

    removed phpadmin

--- phpMyAdmin-4.6.4-all-languages/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$i=0;
-$i++;
-$cfg['Servers'][$i]['user']          = 'weightchallenge';
-$cfg['Servers'][$i]['password']      = 'weightchallenge'; // use here your password
-$cfg['Servers'][$i]['auth_type']     = 'config';
-?>
