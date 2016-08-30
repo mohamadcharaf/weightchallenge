9ce393aa6bc4717297d45797b1249a440e765b56
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-08-30 18:49:49 -0500

    Again

--- dbconfig.php
+++ dbconfig.php
@@ -1,43 +1,17 @@
 <?php
-/*
-if( getenv( 'OPENSHIFT_MYSQL_DB_HOST' ) ){
-  define( 'DB_HOST', getenv( 'OPENSHIFT_MYSQL_DB_HOST' ) );
-  define( 'DB_PORT', getenv( 'OPENSHIFT_MYSQL_DB_PORT' ) );
-  define( 'DB_USER', getenv( 'OPENSHIFT_MYSQL_DB_USERNAME' ) );
-  define( 'DB_PASS', getenv( 'OPENSHIFT_MYSQL_DB_PASSWORD' ) );
-  define( 'DB_SOCK', getenv( 'OPENSHIFT_MYSQL_DB_SOCKET' ) );
-  define( 'DB_URL', getenv( 'OPENSHIFT_MYSQL_DB_URL' ) );
-
-  define( 'DB_NAME', 'eap-app-mysql' );
-}
-else{
-//  define( 'DB_HOST', 'localhost' );
-*/
-  define( 'DB_HOST', '127.0.0.1' );
-  define( 'DB_PORT', '3306' );
-  define( 'DB_USER', 'userx4C' );
-  define( 'DB_PASS', '1huOBjL5' );
-
-  define( 'DB_NAME', 'eap-app-mysql' );
-/*
-//  define( 'DB_SOCK', getenv( 'OPENSHIFT_MYSQL_DB_SOCKET' ) );
-//  define( 'DB_URL', getenv( 'OPENSHIFT_MYSQL_DB_URL' ) );
-}
-*/
 
 
 class Database{
-  private $username = DB_USER;
-  private $password = DB_PASS;
-//  private $host = DB_HOST;
-  private $db_name = DB_NAME;
-//  private $port = DB_PORT;
+  private $username = 'userx4C';
+  private $password = '1huOBjL5';
+  private $host = 'localhost';
+  private $db_name = 'eap-app-mysql';
+  private $port = '3306';
   public $conn;
 
   public function dbConnection(){
     $this->conn = null;
     try{
-//      $this->conn = new PDO( "mysql:host={$this->host};port={$this->port};dbname={$this->db_name}", $this->username, $this->password );
       $this->conn = new PDO( "mysql:host={$this->host};port={$this->port};dbname={$this->db_name}", $this->username, $this->password );
       $this->conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     }
