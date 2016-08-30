c7de5adf29742afc0a4cb6b26b90ac083a6bd7f7
Mohamad Charaf <mohamad.charaf@gmail.com>
2016-08-30 05:27:58 -0500

    config

---
+++ phpMyAdmin-4.6.4-all-languages/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$i=0;
+$i++;
+$cfg['Servers'][$i]['user']          = 'weightchallenge';
+$cfg['Servers'][$i]['password']      = 'weightchallenge'; // use here your password
+$cfg['Servers'][$i]['auth_type']     = 'config';
+?>
