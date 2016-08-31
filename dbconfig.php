74202190ba580f5ce83003e4c046c78b2215be54
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-08-31 12:49:56 -0500

    Use a remote DB to avoid gateway errors

--- dbconfig.php
+++ dbconfig.php
@@ -1,11 +1,17 @@
 <?php
 // Convert from PDO to mysqli because openshift returns either error 504 or 502 with PDO
 class Database{
-
+/*
   private $username = 'userx4C';
   private $password = '1huOBjL5';
   private $host = 'weight-challenge-mysql-weight-challenge.0ec9.hackathon.openshiftapps.com';
   private $db_name = 'eap-app-mysql';
+  private $port = '3306';
+*/
+  private $username = 'openshifty';
+  private $password = 'oneseventwelve22';
+  private $host = 'imadethis.freitag.theinscrutable.us';
+  private $db_name = 'maker';
   private $port = '3306';
   public $conn;
 
