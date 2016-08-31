bd1e5563c9a16ffebe4742813bc4cc3fb588e824
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-08-30 22:38:02 -0500

    Major code cleanup
    Rename user class file
    Move all common HTML to new top/bottom include files
    Isolate to a page what makes it unique

---
+++ common_bottom.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+        <p class="blockquote-reverse" style="margin-top:200px;">
+          This is the RedHat OpenShift Weight Challenge Hackathon App
+          <br />
+          <br />
+          <a href="http://openshift.devpost.com/?ref_content=featured&ref_feature=challenge&ref_medium=portfolio">Code Healthy With OpenShift</a>
+        </p>
+      </div>
+    </div>
+  </div>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
