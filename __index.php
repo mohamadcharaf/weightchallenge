83f29dfca70733809d0d4214c7d970e53fba817c
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-09-01 01:12:48 -0500

    Again

--- __index.php
+++ __index.php
@@ -5,9 +5,10 @@ define( 'DB_PASS', getenv( 'DB_PASS' ) );
 
 echo '<br>Hello World';
 
-echo '<br>DB_HOST: ' . DB_HOST;
+echo '<br>DB_NAME: ' . DB_NAME;
 echo '<br>DB_USER: ' . DB_USER;
 echo '<br>DB_PASS: ' . DB_PASS;
+//echo '<br>DB_PASS: ' . 'No, not going to show you the pw';
 
 phpinfo();
 
