ca1d5b32142ee154e74f6513fd80c24cc509d57d
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-09-01 01:27:26 -0500

    Again

--- dbconfig.php
+++ dbconfig.php
@@ -1,28 +1,15 @@
 <?php
 class Database{
-  private $servername = "mysql";
+  private $servername = 'mysql';
   private $username = 'weightchallenge';
   private $password = 'weightchallenge';
-/*
-  private $host = 'weight-challenge-mysql-weight-challenge.0ec9.hackathon.openshiftapps.com';
-  private $db_name = 'eap-app-mysql';
-  private $port = '3306';
-*/
 
-/*
-  private $username = 'openshifty';
-  private $password = 'oneseventwelve22';
-  private $host = 'imadethis.freitag.theinscrutable.us';
-  private $db_name = 'openshifty';
-  private $port = getenv( 'DB_PORT' );
-*/
   public $conn;
 
   public function dbConnection(){
     $this->conn = null;
     try{
-//      $this->conn = new PDO( "mysql:host={$this->host};port={$this->port};dbname={$this->db_name}", $this->username, $this->password );
-      $this->conn = new PDO( "mysql:dbname={$this->servername}", $this->username, $this->password );
+      $this->conn = new PDO( "mysql:server={$this->servername}", $this->username, $this->password );
       $this->conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     }
     catch( Exception $exception ){
