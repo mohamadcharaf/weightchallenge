68bab4436a64a86a4c7b174c4117e66b5356c795
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-09-02 00:54:46 -0500

    Progress
    Added an "about" page for display of licenses and giving general info on
    the program.
    Added new bootstrap style
    Added DataTables for display of history (and a partial back-end for it)

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php
+require_once( 'session.php' );
+require_once( 'common_top.php' );
+?>
+<p class="h4">About</p>
+<hr>
+<br>Stuff and things here
+<br>This page may not need to be secure.
+<br>If we use that Admin Template theme, but it's required notice here.
+<?php
+require_once( 'common_bottom.php' );
+?>
--- common_top.php
+++ common_top.php
@@ -3,16 +3,24 @@ require_once( 'user.php' );
 $user = new USER();
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html xmlns='http://www.w3.org/1999/xhtml'>
 <head>
   <title>Hello World</title>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <script type="text/javascript" src="jquery-3.1.0.min.js"></script>
-  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-  <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
-  <link rel="stylesheet" href="style.css" type="text/css"  />
-?>
+  <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+  <script type='text/javascript' src='jquery-3.1.0.min.js'></script>
+
+  <script type='text/javascript' src='jquery.dataTables.min.js'></script>
+  <link rel='stylesheet' type='text/css' href='jquery.dataTables.min.css' >
+
+<!--  <link rel="stylesheet" type='text/css' media="screen" href="bootstrap/css/bootstrap.min.css"> -->
+<!--  <link rel="stylesheet" type='text/css' media="screen" href="bootstrap/css/bootstrap-theme.min.css"> -->
+<!--  <link rel="stylesheet" type="text/css" media="screen" href="style.css"> -->
+
+  <link rel='stylesheet' type='text/css' media='screen' href='bootstrap/css/bootstrap.min.css' />
+  <link rel='stylesheet' type='text/css' media='screen' href='bootstrap/css/datepicker3.css' />
+  <link rel='stylesheet' type='text/css' media='screen' href='bootstrap/css/bootstrap-table.css' />
+  <link rel='stylesheet' type='text/css' media='screen' href='bootstrap/css/styles.css' />
 </head>
 <body>
 <nav class="navbar navbar-default navbar-fixed-top">
@@ -23,9 +31,9 @@ $user = new USER();
 if( $user->is_loggedin() ){
 ?>
         <li><a href="home.php"><span class="glyphicon glyphicon-home"></span> home</a></li>
-        <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span> profile</a></li>
         <li><a href="history.php"><span class="glyphicon glyphicon-user"></span> history</a></li>
         <li><a href="challenge.php"><span class="glyphicon glyphicon-user"></span> challenge</a></li>
+        <li><a href="about.php"><span class="glyphicon glyphicon-user"></span> About</a></li>
 <?php
 }
 ?>
