8a890ad0ff65af239379d7a53f304b4e2ca1dbf4
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-09-09 10:36:20 -0500

    Cleanup

--- __index.php
+++ __index.php
@@ -5,14 +5,16 @@ define( 'DB_NAME', getenv( 'DB_NAME' ) );
 define( 'DB_USER', getenv( 'DB_USER' ) );
 define( 'DB_PASS', getenv( 'DB_PASSWORD' ) );
 
-echo '<br>Hello World A';
+echo '<br>Hello World';
 
+/**
 echo '<br>DB_HOST: ' . DB_HOST;
 echo '<br>DB_PORT: ' . DB_PORT;
 echo '<br>DB_NAME: ' . DB_NAME;
 echo '<br>DB_USER: ' . DB_USER;
 //echo '<br>DB_PASS: ' . DB_PASS;
 echo '<br>DB_PASS: ' . 'No, not going to show you the pw!';
+**/
 
 phpinfo();
 
