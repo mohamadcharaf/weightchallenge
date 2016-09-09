58b3870a20739aee633811b440672d5290c57e04
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-09-09 09:17:20 -0500

    Test using openshift DB config

--- __dbconn.php
+++ __dbconn.php
@@ -2,9 +2,18 @@
 //$servername = "eap-app-mysql";
 //$username = "userx4C";
 //$password = "1huOBjL5";
-$servername = "mysql";
-$username = "weightchallenge";
-$password = "weightchallenge";
+//$servername = "mysql";
+//$username = "weightchallenge";
+//$password = "weightchallenge";
+
+define( 'DB_NAME', getenv( 'DB_NAME' ) );
+define( 'DB_USER', getenv( 'DB_USER' ) );
+define( 'DB_PASS', getenv( 'DB_PASS' ) );
+
+$servername = DB_NAME;
+$username = DB_USER;
+$password = DB_PASS;
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -12,6 +21,6 @@ $conn = new mysqli($servername, $username, $password);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 echo "Connected successfully";
 ?>
