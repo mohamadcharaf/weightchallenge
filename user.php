8cb67bd7d695cd634449ac55f4a73e0ba58b7f00
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-09-08 21:33:39 -0500

    Code cleanup
    Code cleanup

--- user.php
+++ user.php
@@ -4,34 +4,31 @@ require_once( 'dbconfig.php' );
 class USER{
   private $conn;
   public  $uname;
+  public  $uid;
 
   public function __construct( $uname = null, $session = null ){
-//echo '<script>console.log( "user constructor" )</script>';
     $database = new Database();
-    $db = $database->dbConnection();
-    $this->conn = $db;
+    $this->conn = $database->dbConnection();
 
     if( $uname == null && $session == null && $this->is_loggedin() ){
-//echo '<script>console.log( "user constructor condition 1" )</script>';
-//echo '<script>console.log( "user constructor user name ' . $_SESSION[ 'user_name' ] . '" )</script>';
       $this->uname = $_SESSION[ 'user_name' ];
       $this->session = $_SESSION[ 'user_session' ];
+//      $this->hasSession( $this->uname, $this->session );  // go get the user_id
+//QQQ work around
+$this->uid = $this->session;
     }
     else if( $uname != null && $session != null && $this->hasSession( $uname, $session ) ){
-//echo '<script>console.log( "user constructor condition 2" )</script>';
       $this->uname = $uname;
       $this->session = $session;
     }
-//else{
-//echo '<script>console.log( "user constructor fall through" )</script>';
-//}
-
   }
 
+/* this is not needed and even if it was, should be in the database class.
   public function runQuery( $sql ){
-    $stmt = $this->conn->prepare($sql);
+    $stmt = $this->conn->prepare( $sql );
     return $stmt;
   }
+*/
 
   public function register( $uname, $umail, $upass ){
     try{
@@ -61,6 +58,7 @@ class USER{
       if( $stmt->rowCount() == 1 ){
         if( password_verify( $upass, $userRow[ 'user_pass' ] ) ){
             $this->uname = $uname;
+// QQQ OK, stop using the user_id as the user_session so that the value changes from time to time in the cookie
             $_SESSION[ 'user_session' ] = $userRow['user_id'];
             $_SESSION[ 'user_name' ] = $uname;
             return true;
@@ -81,14 +79,19 @@ class USER{
 
 
   // For any given user name and session ID this tests for validity. (Can be used in back end calls too)
+//QQQ requires session_id in the user table!!
   public function hasSession( $uname, $user_session ){
-echo '<script>console.log( "Checking hasSession()" )</script>';
     try{
-      $stmt = $this->conn->prepare( "SELECT count(*) FROM users WHERE sessionid = ? AND user_name = ?" );
-      $stmt->bind_param( 'ss', $user_session, $uname );
+      $stmt = $this->conn->prepare( "SELECT count(*) FROM users WHERE sessionid = :user_session AND user_name = :uname" );
+      $stmt->bindparam( ":user_session", $user_session );
+      $stmt->bindparam( ":uname", $uname );
       $stmt->execute();
 
       if( $stmt->fetchColumn() == 1 ){
+        $stmt2 = $this->conn->prepare( "SELECT user_id FROM users WHERE sessionid = ? AND user_name = ?" );
+        $stmt2->bind_param( 'ss', $user_session, $uname );
+        $stmt2->execute();
+        $this->uid = $stmt2->fetchColumn( 0 );
         // one row, one column in results.  Value should be 0 or 1.
         return true;
       }
