1c217340788c5397a224c68c94acade55ab10ec9
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-09-09 09:22:54 -0500

    Adding test for PDO connection
    Adding test for PDO connection

--- __dbconn.php
+++ __dbconn.php
@@ -1,4 +1,12 @@
 <?php
+define( 'DB_NAME', getenv( 'DB_NAME' ) );
+define( 'DB_USER', getenv( 'DB_USER' ) );
+define( 'DB_PASS', getenv( 'DB_PASS' ) );
+
+
+/**
+// Try connection with mysqli
+
 //$servername = "eap-app-mysql";
 //$username = "userx4C";
 //$password = "1huOBjL5";
@@ -6,9 +14,6 @@
 //$username = "weightchallenge";
 //$password = "weightchallenge";
 
-define( 'DB_NAME', getenv( 'DB_NAME' ) );
-define( 'DB_USER', getenv( 'DB_USER' ) );
-define( 'DB_PASS', getenv( 'DB_PASS' ) );
 
 $servername = DB_NAME;
 $username = DB_USER;
@@ -16,11 +21,40 @@ $password = DB_PASS;
 
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli( $servername, $username, $password );
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if( $conn->connect_error ){
+  die( "MySQLi Connection failed: " . $conn->connect_error );
 }
-echo "Connected successfully";
+echo "MySQLi Connected successfully.";
+**/
+
+// Try connection with PDO
+class Database{
+  private $servername = DB_NAME;
+  private $username = DB_USER;
+  private $password = DB_PASS;
+
+  public $conn;
+
+  public function dbConnection(){
+    $this->conn = null;
+    try{
+      $this->conn = new PDO( "mysql:host=localhost;dbname={$this->dbname}", $this->username, $this->password );
+
+      $this->conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+      echo "PDO Connected successfully!";
+    }
+    catch( Exception $exception ){
+      echo 'PDO Connection error: ' . $exception->getMessage();
+      die();
+    }
+
+    return $this->conn;
+  }
+}
+
+$db = new Database();
+$db->dbConnection();
 ?>
