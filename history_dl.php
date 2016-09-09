1619764d692982808b5ee4bbc9e19fc1976fc462
ThermoMan <thestalwart1-tstat@yahoo.com>
2016-09-09 13:33:39 -0500

    Can you hear me now?

--- history_dl.php
+++ history_dl.php
@@ -16,9 +16,9 @@ if( $uid == '' ){
   return;
 }
 
-$draw = (int) (isset($_REQUEST['draw'])) ? htmlspecialchars($_REQUEST['draw']) : 1;
+$draw = (int) ( (isset($_REQUEST['draw'])) ? htmlspecialchars($_REQUEST['draw']) : 1 );
 $start = (int) ( (isset($_REQUEST['start'])) ? htmlspecialchars($_REQUEST['start']) : 0 );
-$length = (int) (isset($_REQUEST['length'])) ? htmlspecialchars($_REQUEST['length']) : 10;
+$length = (int) ( (isset($_REQUEST['length'])) ? htmlspecialchars($_REQUEST['length']) : 10 );
 //if( $length == -1 ){ $length = count( $foo_json['data'] ); }
 //$search = (isset($_REQUEST['search'])) ? $_REQUEST['search'] : null;
 
@@ -62,12 +62,8 @@ $sql_string = '
 
 $stmt = $pdo->prepare( $sql_string );
 $stmt->bindParam( ':uid', $uid, PDO::PARAM_INT );
-//$stmt->bindParam( ':start', intval($start), PDO::PARAM_INT );   // Paging support
-//$stmt->bindParam( ':length', intval($length), PDO::PARAM_INT ); // Paging support
-$foo = 0;
-$bar = 25;
 $stmt->bindParam( ':start', $start, PDO::PARAM_INT );   // Paging support
-$stmt->bindParam( ':length', $bar, PDO::PARAM_INT ); // Paging support
+$stmt->bindParam( ':length', $length, PDO::PARAM_INT ); // Paging support
 $stmt->execute();
 
 $allData = $stmt->fetchAll( PDO::FETCH_NUM );
